232fb8e61af984ba7f8b36ea2d1cadce27379716
Jacob <jamsbury95@hotmail.com>
2022-05-31 21:22:36 -0700

    Worked half way through testing

--- python/code_challenges/linked_list_implementation/linked_list.py
+++ python/code_challenges/linked_list_implementation/linked_list.py
@@ -1,16 +1,29 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 class LinkedList:
     """
-    Put docstring here
+    code to initialize, insert, stringify, and check linked list.
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, head=None):
+        self.head = head
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        node = Node(value)
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
 
+    def to_string(self):
+        if self.head is None:
+            return "Null"
+        
+
+    def includes(self):
+        pass
 
 class TargetError:
     pass
