6e8df69088e7968d0485fbdc2b78655a9692b3f1
Jacob Amsbury <jamsbury95@hotmail.com>
2022-07-18 21:42:02 -0700

    could not figure out the clone process

---
+++ python/code_challenges/tree/queue.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self, value, next = None):
+        self.value = value
+        self.next = next
+
+class Queue:
+    """
+    Put docstring here
+    """
+
+    def __init__(self):
+        self.front = None
+        self.back = None
+
+    def __str__(self):
+        current = self.front
+        print(current.value)
+        str = ''
+        while current:
+            str += f'{{ {current.value} }} -> '
+            current = current.next
+        return str + "NULL"
+
+    def enqueue(self, value):
+        node = Node(value)
+        if self.front is None:
+            self.front = node
+            self.back = node
+        else:
+            self.back.next = node
+            self.back = node
+
+    def dequeue(self):
+        if self.front is None:
+            raise InvalidOperationError
+        temp = self.front
+        if self.front == self.back:
+            self.back = None
+        self.front = self.front.next
+        return temp.value
+
+    def peek(self):
+        if self.front is None:
+            raise InvalidOperationError
+        else:
+            return self.front.value
+
+    def is_empty(self):
+        if self.front:
+            return False
+        else:
+            return True
+
+if __name__ == "__main__":
+    q = Queue()
+    q.enqueue(1)
+    q.enqueue(2)
+    q.dequeue()
+    q.dequeue()
+    print(q.front)
+    print("empty", q.is_empty())
--- python/code_challenges/tree/tree_fizz_buzz.py
+++ python/code_challenges/tree/tree_fizz_buzz.py
@@ -1,18 +1,46 @@
 from binary_tree import BinaryTree
+from queue import Queue
+from kary_tree import Node
 
-# if root.child % 15 = 1:
-#     return "fizzbuzz"
-# elif root.child % 3 = 1:
-#     return "fizz"
-# elif root.child % 5 = 1:
-#     return "buzz"
-# else:
-#     return str(root.child)
+def fizz_buzz_tree(tree, Node = None):
+    new_tree = tree
+    if not tree.root:
+        return "no tree found"
 
-def fizz_buzz_tree(tree):
-    new_tree = KaryTree()
-    if not root:
-        return
-    else:
-        return "Let\'s begin!"
+    queue = Queue()
 
+    queue.enqueue(new_tree.root)
+
+    while not queue.is_empty():
+        node = queue.dequeue()
+        if node.value % 15 == 0:
+            node.value = "FizzBuzz"
+        elif node.value % 3 == 0:
+            node.value = "Fizz"
+        elif node.value % 5 == 0:
+            node.value = "Buzz"
+        else:
+            node.value = str(node.value)
+
+        for child in node.children:
+            queue.enqueue(child)
+
+    return new_tree
+
+
+if __name__ == "__main__":
+    one = Node(1)
+    two = Node(2)
+    three = Node(3)
+    four = Node(4)
+    five = Node(5)
+    six = Node(6)
+    seven = Node(7)
+    eight = Node(8)
+    nine = Node(9)
+    ten = Node(10)
+    eleven = Node(11)
+    twelve = Node(12)
+    thirteen = Node(13)
+    fourteen = Node(14)
+    fifteen = Node(15)
